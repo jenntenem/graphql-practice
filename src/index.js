457697f4e6c112592432a7285257581b5434e804
Jenniffer Tenempaguay <jenntenem@gmail.com>
2023-10-28 01:35:28 -0500

    API-Wide authorization Example

--- src/index.js
+++ src/index.js
@@ -50,3 +50,26 @@ app.use(
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+/*
+API-Wide authorization
+
+const server = new ApolloServer({
+ typeDefs,
+ resolvers,
+ context: ({ req }) => {
+ // get the user token from the headers
+ const token = req.headers.authorization || '';
+
+ // try to retrieve a user with the token
+ // this function could decode the token and look up the user in the actual database
+ const user = getUser(token);
+
+ // optionally block the user
+ // you could also check user roles/permissions here
+ if (!user) throw new AuthenticationError('you must be logged in');
+
+ // add the user object to the context
+ return { user };
+},
+*/
